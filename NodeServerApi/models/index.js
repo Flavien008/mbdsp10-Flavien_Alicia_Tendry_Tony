1c84dd92db741f87e4c05750a1e0d491c8e7c63f
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-08-06 22:50:59 +0300

    Update index.js

--- NodeServerApi/models/index.js
+++ NodeServerApi/models/index.js
@@ -1,25 +1,45 @@
 const sequelize = require('../config/database');
 const Utilisateur = require('./utilisateur');
+const Role = require('./role');
 const Poste = require('./poste');
 const Postedetails = require('./postedetails');
 const Objet = require('./objet');
-const Categorie = require('./categorie'); 
+const Categorie = require('./categorie');
 
-// Importer tous les modèles
+// Import all models
 const models = {
     Utilisateur,
+    Role,
     Poste,
     Postedetails,
     Objet,
     Categorie
 };
 
-// Définir les associations
+// Define associations
 Object.keys(models).forEach(modelName => {
     if (models[modelName].associate) {
         models[modelName].associate(models);
     }
 });
+
+// Synchronize models in the correct order
+async function syncModels() {
+    try {
+        // Synchronize base tables first
+        await Role.sync({ alter: true });
+        await Categorie.sync({ alter: true });
+        await Utilisateur.sync({ alter: true });
+        await Poste.sync({ alter: true });
+        await Objet.sync({ alter: true });
+        await Postedetails.sync({ alter: true });
+        console.log('Database & tables created!');
+    } catch (error) {
+        console.error('Error syncing models:', error);
+    }
+}
+
+syncModels();
 
 const db = {
     ...models,
