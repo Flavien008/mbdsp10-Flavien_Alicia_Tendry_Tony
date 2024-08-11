fc27491e9ed45d516b1a400ee63914824345a22e
tonyfenoaina <tfenoaina@gmail.com>
2024-08-11 23:01:05 +0300

    integration login

--- TPTMBDS/app/src/main/java/com/example/tpt_mbds/service/UserService.java
+++ TPTMBDS/app/src/main/java/com/example/tpt_mbds/service/UserService.java
@@ -1,6 +1,7 @@
 package com.example.tpt_mbds.service;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.util.Log;
 
 import com.android.volley.Request;
@@ -16,11 +17,14 @@ import org.json.JSONObject;
 public class UserService {
 
     private static final String SIGNUP_URL = ApiConfig.BASE_URL + "/users/signup";
+    private static final String LOGIN_URL = ApiConfig.BASE_URL + "/users/login";
 
     private RequestQueue requestQueue;
+    private SharedPreferences sharedPreferences;
 
     public UserService(Context context) {
         requestQueue = Volley.newRequestQueue(context);
+        sharedPreferences = context.getSharedPreferences("UserPrefs", Context.MODE_PRIVATE);
     }
 
     public void signup(String username, String email, String dateNaissance, int roleId, String password, final SignupCallback callback) {
@@ -53,6 +57,51 @@ public class UserService {
     }
 
     public interface SignupCallback {
+        void onSuccess(JSONObject response);
+        void onError(VolleyError error);
+    }
+
+    public void login(String username, String password, final LoginCallback callback) {
+        JSONObject requestBody = new JSONObject();
+        try {
+            requestBody.put("username", username);
+            requestBody.put("password", password);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.POST, LOGIN_URL, requestBody,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject userObject = response.getJSONObject("user");
+                            String token = response.getString("token");
+
+                            // Save user info and token to SharedPreferences
+                            SharedPreferences.Editor editor = sharedPreferences.edit();
+                            editor.putInt("userId", userObject.getInt("id"));
+                            editor.putString("username", userObject.getString("name"));
+                            editor.putString("token", token);
+                            editor.apply();
+
+                            callback.onSuccess(response);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        callback.onError(error);
+                    }
+                });
+
+        requestQueue.add(request);
+    }
+
+    public interface LoginCallback {
         void onSuccess(JSONObject response);
         void onError(VolleyError error);
     }
