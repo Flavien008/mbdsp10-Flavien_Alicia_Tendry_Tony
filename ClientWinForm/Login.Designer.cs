a6b9c4fcad5e41dad7ae0e36d1ccdce57d8ed771
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-08-13 15:15:59 +0300

    Login page

--- ClientWinForm/Login.Designer.cs
+++ ClientWinForm/Login.Designer.cs
@@ -37,14 +37,14 @@
             // 
             // textBoxEmail
             // 
-            this.textBoxEmail.Location = new System.Drawing.Point(130, 50);
+            this.textBoxEmail.Location = new System.Drawing.Point(157, 50);
             this.textBoxEmail.Name = "textBoxEmail";
             this.textBoxEmail.Size = new System.Drawing.Size(200, 22);
             this.textBoxEmail.TabIndex = 0;
             // 
             // textBoxPassword
             // 
-            this.textBoxPassword.Location = new System.Drawing.Point(130, 100);
+            this.textBoxPassword.Location = new System.Drawing.Point(157, 100);
             this.textBoxPassword.Name = "textBoxPassword";
             this.textBoxPassword.PasswordChar = '*';
             this.textBoxPassword.Size = new System.Drawing.Size(200, 22);
@@ -55,33 +55,34 @@
             this.labelEmail.AutoSize = true;
             this.labelEmail.Location = new System.Drawing.Point(50, 50);
             this.labelEmail.Name = "labelEmail";
-            this.labelEmail.Size = new System.Drawing.Size(73, 16);
+            this.labelEmail.Size = new System.Drawing.Size(70, 16);
             this.labelEmail.TabIndex = 2;
-            this.labelEmail.Text = "Username:";
+            this.labelEmail.Text = "Username";
             // 
             // labelPassword
             // 
             this.labelPassword.AutoSize = true;
             this.labelPassword.Location = new System.Drawing.Point(50, 100);
             this.labelPassword.Name = "labelPassword";
-            this.labelPassword.Size = new System.Drawing.Size(70, 16);
+            this.labelPassword.Size = new System.Drawing.Size(67, 16);
             this.labelPassword.TabIndex = 3;
-            this.labelPassword.Text = "Password:";
+            this.labelPassword.Text = "Password";
             // 
             // buttonLogin
             // 
-            this.buttonLogin.Location = new System.Drawing.Point(130, 150);
+            this.buttonLogin.Location = new System.Drawing.Point(157, 156);
             this.buttonLogin.Name = "buttonLogin";
             this.buttonLogin.Size = new System.Drawing.Size(200, 23);
             this.buttonLogin.TabIndex = 4;
             this.buttonLogin.Text = "Se connecter";
             this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
             // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(400, 250);
+            this.ClientSize = new System.Drawing.Size(470, 291);
             this.Controls.Add(this.buttonLogin);
             this.Controls.Add(this.labelPassword);
             this.Controls.Add(this.labelEmail);
--- ClientWinForm/Login.cs
+++ ClientWinForm/Login.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Configuration;
 
 namespace ClientWinForm
 {
@@ -16,5 +19,55 @@ namespace ClientWinForm
         {
             InitializeComponent();
         }
+
+        private async void buttonLogin_Click(object sender, EventArgs e)
+        {
+            string email = textBoxEmail.Text;
+            string password = textBoxPassword.Text;
+
+            if (string.IsNullOrEmpty(email) || string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show("Please enter both email and password.");
+                return;
+            }
+
+            string baseUri = ConfigurationManager.AppSettings["BaseUri"];
+            MessageBox.Show("baseUri" + baseUri);
+            using (HttpClient client = new HttpClient())
+            {
+                var loginData = new
+                {
+                    username = email,
+                    password = password
+                };
+
+                var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
+
+                HttpResponseMessage response = await client.PostAsync($"{baseUri}/users/login", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseData = await response.Content.ReadAsStringAsync();
+                    var result = JsonConvert.DeserializeObject<LoginResponse>(responseData);
+
+                    string authToken = result.Token;
+                    MessageBox.Show("tokenn"+authToken);
+                    this.Hide();
+
+                    //MainForm mainForm = new MainForm(authToken);
+                    //mainForm.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Login failed. Please check your credentials.");
+                }
+            }
+        }
+
+
+        public class LoginResponse
+        {
+            public string Token { get; set; }
+        }
+
     }
 }
