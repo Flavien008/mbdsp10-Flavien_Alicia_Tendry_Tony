7cf4f0a3dd7985eaa01701b7f624525f782e1bd5
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-08-13 19:41:02 +0300

    base en cloud

--- NodeServerApi/config/database.js
+++ NodeServerApi/config/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('TPT', 'postgres', 'root', {
-    host: 'localhost',
+const sequelize = new Sequelize('TPT', 'postgres', 'tptmbds2024', {
+    host: '34.44.27.115',
     dialect: 'postgres',
 });
 
