11e709ed213e99f673ac5f41af82afc2b1a87e7a
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-08-13 19:31:47 +0300

    btn deco

--- ClientWinForm/MainForm.Designer.cs
+++ ClientWinForm/MainForm.Designer.cs
@@ -1,4 +1,6 @@
-﻿namespace ClientWinForm
+﻿using System;
+
+namespace ClientWinForm
 {
     partial class MainForm
     {
@@ -32,13 +34,48 @@
         /// Required method for Designer support - do not modify
         /// the contents of this method with the code editor.
         /// </summary>
+        /// 
+        private void buttonLogout_Click(object sender, EventArgs e)
+        {
+            // Afficher à nouveau le formulaire de connexion
+            Login loginForm = new Login();
+            loginForm.Show();
+
+            // Fermer la MainForm actuelle
+            this.Close();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // Charger les données initiales ici
+        }
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.buttonLogout = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonLogout
+            // 
+            this.buttonLogout.Location = new System.Drawing.Point(12, 12);
+            this.buttonLogout.Name = "buttonLogout";
+            this.buttonLogout.Size = new System.Drawing.Size(100, 30);
+            this.buttonLogout.TabIndex = 0;
+            this.buttonLogout.Text = "Déconnexion";
+            this.buttonLogout.UseVisualStyleBackColor = true;
+            this.buttonLogout.Click += new System.EventHandler(this.buttonLogout_Click);
+            // 
+            // MainForm
+            // 
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Accueil";
+            this.Controls.Add(this.buttonLogout);
+            this.Name = "MainForm";
+            this.Text = "Main Application";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+
         }
+
+        private System.Windows.Forms.Button buttonLogout;
 
         #endregion
     }
