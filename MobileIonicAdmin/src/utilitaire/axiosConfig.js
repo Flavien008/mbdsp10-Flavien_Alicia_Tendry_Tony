98069342bd7ee9ab2704b82de0d58b7c20ba8f8b
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-08-23 10:49:06 +0300

    api en ligne

--- MobileIonicAdmin/src/utilitaire/axiosConfig.js
+++ MobileIonicAdmin/src/utilitaire/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8010/api',
+  baseURL: 'https://mbdsp10-flavien-alicia-tendry-tony.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
