2b8062424ae695c9d18697866bcbea58097af0e3
Flavien Rakotoarison <72599570+Flavien008@users.noreply.github.com>
2024-08-26 20:40:58 +0300

    update lien api for ionic admin app

--- MobileIonicAdmin/src/utilitaire/axiosConfig.js
+++ MobileIonicAdmin/src/utilitaire/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'https://mbdsp10-flavien-alicia-tendry-tony.onrender.com/api',
+  baseURL: 'https://modular-analog-431619-t6.oa.r.appspot.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
