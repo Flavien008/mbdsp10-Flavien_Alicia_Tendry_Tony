3bb74cc22111f053af18511b3a36964997da73b0
Tendry Rakotoarivony <80846844+TendryArivony@users.noreply.github.com>
2024-08-26 11:34:03 +0300

    Personne connecté

--- Front_Angular/src/app/shared/account-breadcrumbs/account-breadcrumbs.component.ts
+++ Front_Angular/src/app/shared/account-breadcrumbs/account-breadcrumbs.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../auth.service'; // Assurez-vous que le chemin est correct
 
 @Component({
   selector: 'app-account-breadcrumbs',
   templateUrl: './account-breadcrumbs.component.html',
   styleUrls: ['./account-breadcrumbs.component.scss']
 })
-
-// AccountBreadcrumbs Component
 export class AccountBreadcrumbsComponent implements OnInit {
+  currentUser: any = null; // Stocke les informations de l'utilisateur connecté
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.currentUser = this.authService.getCurrentUser(); // Récupère l'utilisateur connecté depuis le service d'authentification
   }
 }
